4991182e9bff671af8fd59a30c39a8d14d384b93
Quentin <lecompte.quentin@gmail.com>
2020-11-16 14:04:54 +0100

    First workshop commit

---
+++ src/main/java/epsi/project/workshop/App.java
@@ -0,0 +1,13 @@
+package epsi.project.workshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
